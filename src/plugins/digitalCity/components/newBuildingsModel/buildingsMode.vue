8c04c43d84b9543312a1cb2cb520912b8e3fedc8
hawk86104 <hawk86104@163.com>
2025-01-08 10:03:22 +0800

    上海城市模型 本地化

--- src/plugins/digitalCity/components/newBuildingsModel/buildingsMode.vue
+++ src/plugins/digitalCity/components/newBuildingsModel/buildingsMode.vue
@@ -29,7 +29,7 @@ import importantBuildings from './importantBuildings.vue'
 
 initMeshBvh()
 const { scene }
-	= await useGLTF('https://opensource-1314935952.cos.ap-nanjing.myqcloud.com/model/digitalCity/shanghaiDraco.gltf',
+	= await useGLTF('./plugins/digitalCity/model/shanghaiDraco/shanghaiDraco.gltf',
 		{ draco: true, decoderPath: './draco/' },
 	)
 // console.log(scene)
