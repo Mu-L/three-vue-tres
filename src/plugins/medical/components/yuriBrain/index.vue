607828e07a8e2d443b9842d2cb1c4d6c7e4241da
hawk86104 <hawk86104@163.com>
2025-01-09 12:30:01 +0800

    完成 yuriBrain  等待封装

---
+++ src/plugins/medical/components/yuriBrain/index.vue
@@ -0,0 +1,14 @@
+<template>
+    <TresGroup>
+        <brainParticles :brainCurves="brainCurves" />
+        <tubes :brainCurves="brainCurves" />
+    </TresGroup>
+</template>
+
+<script setup lang="ts">
+import { getBrainCurves } from './makeData'
+import brainParticles from './brainParticles.vue'
+import tubes from './tubes.vue'
+
+const brainCurves = getBrainCurves() as any
+</script>
---
+++ src/plugins/medical/pages/yuriBrain.vue
@@ -0,0 +1,34 @@
+<!--
+ * @Description: 
+ * @Version: 1.668
+ * @Autor: 地虎降天龙
+ * @Date: 2025-01-09 10:24:46
+ * @LastEditors: 地虎降天龙
+ * @LastEditTime: 2025-01-09 12:29:28
+-->
+<template>
+	<TresCanvas clearColor="#000" window-size>
+			<TresPerspectiveCamera :position="[6, 3, 0]" :fov="45" :near="0.1" :far="10000" />
+			<OrbitControls enableDamping autoRotate />
+
+			<yuriBrainModel />
+
+			<Suspense>
+					<reflectorDUDV :position="[0, -1.36, 0]" v-bind="reflectorState" />
+			</Suspense>
+	</TresCanvas>
+</template>
+
+<script setup lang="ts">
+import { reactive } from 'vue'
+import { OrbitControls } from '@tresjs/cientos'
+import { reflectorDUDV } from 'PLS/floor'
+import yuriBrainModel from '../components/yuriBrain/index.vue'
+
+const reflectorState = reactive({
+	reflectivity: 0.8,
+	showGridHelper: false,
+	scale: 1,
+})
+
+</script>
