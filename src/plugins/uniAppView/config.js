66914bd3938be27ef139b23cfdce1996961bf81c
hawk86104 <hawk86104@163.com>
2025-03-06 18:53:21 +0800

    调试页面 编写中

---
+++ src/plugins/uniAppView/config.js
@@ -0,0 +1,22 @@
+export default {
+    name: 'uniAppView',
+    title: 'uniapp小程序',
+    intro: '结合uni-app的开源生态，使用web-view的方式和uni-app交互，方便在您的微信、支付宝等小程序、以及安卓、ios中插入我们的tvt案例以及插件',
+    version: '1.0.0',
+    author: '地虎降天龙',
+    website: 'https://gitee.com/hawk86104',
+    state: 'active',
+    creatTime: '2025-03-05',
+    updateTime: '2025-03-05',
+    require: [],
+    preview: [
+        {
+            src: 'plugins/basic/base/preview/theBasic.png',
+            type: 'img',
+            name: 'h5demo',
+            title: '简单UI交互',
+            disableFPSGraph: true,
+            disableSrcBtn: true,
+        },
+    ],
+}
---
+++ src/plugins/uniAppView/pages/h5demo.vue
@@ -0,0 +1,49 @@
+<!--
+ * @Description: 
+ * @Version: 1.668
+ * @Autor: 地虎降天龙
+ * @Date: 2025-03-05 10:47:55
+ * @LastEditors: 地虎降天龙
+ * @LastEditTime: 2025-03-05 12:02:41
+-->
+<template>
+    <div class="title">dsad</div>
+    <div class="btn-list">
+        <button class="btn" type="button" data-action="navigateTo">navigateTo</button>
+        <button class="btn" type="button" data-action="redirectTo">redirectTo</button>
+        <button class="btn" type="button" data-action="navigateBack">navigateBack</button>
+        <button class="btn" type="button" data-action="reLaunch">reLaunch</button>
+        <button class="btn btn-red" type="button" data-action="switchTab">switchTab</button>
+        <button class="btn btn-yellow" type="button" @click="postMessage">postMessage</button>
+        <button class="btn btn-yellow" type="button" data-action="getEnv">getEnv</button>
+    </div>
+</template>
+
+<script setup lang="ts">
+import { onMounted } from 'vue'
+import { loadJweixin, loadWebView } from '../lib/initScript'
+
+declare const uni: any
+
+onMounted(async () => {
+    console.log('onMounted')
+    await loadJweixin()
+    await loadWebView()
+    uni.getEnv((res: any) => {
+        alert(`当前环境：${JSON.stringify(res)}`)
+    })
+})
+
+const postMessage = () => {
+    uni.postMessage({
+        data: {
+            action: '发送消息了',
+        },
+    })
+}
+</script>
+<style lang="less" scoped>
+.title {
+    color: white;
+}
+</style>
