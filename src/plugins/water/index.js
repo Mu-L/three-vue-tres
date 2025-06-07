9f16f83dc37120986e13ede70b82d2dbe2cc24e3
hawk86104 <hawk86104@163.com>
2025-06-07 08:15:34 +0800

    no message

---
+++ src/plugins/water/index.js
@@ -0,0 +1,11 @@
+/*
+ * @Description: 
+ * @Version: 1.668
+ * @Autor: 地虎降天龙
+ * @Date: 2025-06-07 08:15:08
+ * @LastEditors: 地虎降天龙
+ * @LastEditTime: 2025-06-07 08:15:11
+ */
+import staticWater from './components/staticWater.vue'
+
+export { staticWater }
