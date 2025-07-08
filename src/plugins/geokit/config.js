18cf4859030540f054af3da28e7c320bcd0f45f6
a876691666 <928200728@qq.com>
2025-07-09 00:17:02 +0800

    feat: 更新geokit的说明文字

--- src/plugins/geokit/config.js
+++ src/plugins/geokit/config.js
@@ -1,7 +1,10 @@
 export default {
-    name: 'geokit',
+    name: '@icegl/geokit',
     title: '地理渲染开发工具',
-    intro: '地理渲染开发工具的使用案例',
+    intro: `由TvT团队推出的 @icegl/geokit 地理渲染开发工具的使用案例
+<br/>🚀高性能, 🚀高易用, 🚀高可扩展性, 还有最最重要的<strong>低复杂度!</strong>
+<br/>仅需掌握几个核心组件的参数，即可进行可靠的3D地理渲染的开发
+<br/>是否在为cesium复杂的sdk烦恼? 是否在为地图api融合threejs头疼? 是否在为threejs加载模型对不齐难受? 这个工具就是你的不二之选`,
     version: '0.0.1',
     author: '石头web',
     website: 'https://github.com/a876691666',
@@ -12,6 +15,6 @@ export default {
     tvtstore: 'LISENCE',
     preview: [
         { src: 'plugins/geokit/preview/case-3dtiles.jpg', type: 'img', name: 'case-3dtiles', title: '加载3dtiles，并控制视点' },
-        { src: 'plugins/geokit/preview/tiandituCase.jpg', type: 'img', name: 'tiandituCase', title: '天地图底图组件的详细示例' },
+        { src: 'plugins/geokit/preview/case-tianditu.jpg', type: 'img', name: 'tiandituCase', title: '天地图底图组件的详细示例' },
     ],
 }
