e026d6c3b9a5b412d306bff4e0940be2401a524d
hawk86104 <hawk86104@163.com>
2025-08-06 09:41:37 +0800

    feat: 增加全局获得当前插件的配置

--- src/components/forPreview/suspenseLayout.vue
+++ src/components/forPreview/suspenseLayout.vue
@@ -4,7 +4,7 @@
  * @Autor: 地虎降天龙
  * @Date: 2023-10-21 17:24:44
  * @LastEditors: 地虎降天龙
- * @LastEditTime: 2025-07-24 09:53:00
+ * @LastEditTime: 2025-08-06 09:24:58
 -->
 <template>
     <Suspense>
@@ -22,6 +22,12 @@ import referenceSource from './referenceSource.vue'
 import FPSGraph from './FPSGraph.vue'
 import miniBts from './miniBts.vue'
 
+declare global {
+  interface Window {
+    tvtPluginConfig?: any
+  }
+}
+
 const originalUrl = window.location.href.split('?')[0]
 const hashPart = originalUrl.split('#')[1] || ''
 const parts = hashPart.split('/')
@@ -33,5 +39,7 @@ if (parts[2] === 'basic') {
 }
 
 console.log('插件配置:', config)
+window.tvtPluginConfig = config
+
 const referenceSourceConfig = config?.preview?.referenceSource
 </script>
