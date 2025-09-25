de46f27bcdfbb7f6f6045cb429c1ac9871019ac7
hawk86104 <hawk86104@163.com>
2025-09-25 16:21:53 +0800

    no message

--- src/plugins/UIdemo/components/svgCom.vue
+++ src/plugins/UIdemo/components/svgCom.vue
@@ -1,16 +1,15 @@
 <template>
-</template>
-
-<!-- <template>
-    <TresGroup>
-        <Suspense>
-            <SVG :scale="0.05" :position="[-1.5, 2, 0]" :src="src" :skipStrokes="skipStrokes" :skipFills="skipFills"  />
-        </Suspense>
+    <TresGroup v-if="!isLoading">
+        <TresMesh v-for="(layer, index) in visibleLayers" :key="`layer-${index}`" :geometry="layer.geometry"
+            :render-order="index">
+            <TresMeshBasicMaterial v-bind="layer.material" />
+        </TresMesh>
     </TresGroup>
-</template>
 
+</template>
 <script setup lang="ts">
-import { SVG } from '@tresjs/cientos'
+import { onUnmounted, computed, watch, ref } from 'vue'
+import { useSVG } from '@tresjs/cientos'
 
 const props = defineProps({
     src: {
@@ -23,4 +22,33 @@ const props = defineProps({
         default: false,
     },
 })
-</script> -->
+
+const layers = ref([]) as any
+const isLoading = ref(true)
+let dispose = ref(() => { }) as any
+
+const visibleLayers = computed(() => {
+    return layers.value.filter((layer: any) => {
+        if (layer.isStroke && !props.skipStrokes) return true
+        if (!layer.isStroke && !props.skipFills) return true
+        return false
+    })
+})
+
+watch(() => props.src, (src) => {
+    dispose.value()
+    const newSvg = useSVG(src)
+    isLoading.value = true
+    watch(() => newSvg.isLoading.value, (il) => {
+        if (!il) {
+            layers.value = newSvg.layers.value
+            isLoading.value = newSvg.isLoading.value
+            dispose.value = newSvg.dispose
+        }
+    })
+}, { immediate: true })
+
+onUnmounted(() => {
+    dispose.value()
+})
+</script>
