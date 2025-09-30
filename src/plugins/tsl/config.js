274f4d4647ea7c5b7a8dc611798f27c74c35f9c1
a876691666 <928200728@qq.com>
2025-09-30 23:50:50 +0800

    feat: 增加基础TSL案例

---
+++ src/plugins/tsl/config.js
@@ -0,0 +1,13 @@
+export default {
+	"name": "tsl",
+	"title": "TSL",
+	"intro": "TSL 是 Three shader Language 的缩写，是一种基于 GLSL & WGSL 的着色器语言，旨在简化着色器的编写和管理。",
+	"version": "0.0.1",
+	"author": "石头web",
+	"website": "https://github.com/a876691666",
+	"state": "active",
+	"require": [],
+	"preview": [
+		{ "src": "plugins/tsl/preview/basicTsl.png", "type": "img", "name": "basicTsl", "title": "最基本的TSL应用" },
+	]
+}
