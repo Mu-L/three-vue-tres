5a23e30eecea8119d9432ad31d4a143f09decd2f
hawk86104 <hawk86104@163.com>
2025-11-06 09:21:10 +0800

    no message

--- src/plugins/zone3Deditor/config.js
+++ src/plugins/zone3Deditor/config.js
@@ -4,7 +4,7 @@
  * @Autor: 地虎降天龙
  * @Date: 2025-05-14 15:15:24
  * @LastEditors: 地虎降天龙
- * @LastEditTime: 2025-10-22 10:23:13
+ * @LastEditTime: 2025-11-06 09:17:17
  */
 export default {
     name: 'zone3Deditor',
@@ -20,12 +20,12 @@ export default {
     QA问答:<br>
     1、导出插件包安装后，需要再安装下免费插件依赖： <a style="color: #5384ff;" href="https://www.icegl.cn/tvtstore/useViewportGizmo" target="_blank">ViewportGizmo插件</a><br>
     此插件会和tvt.js生态持续协同，不断加入组件，用于您项目的快速落地<br>`,
-    version: '2.1.0',
+    version: '2.1.2',
     author: '地虎降天龙',
     website: 'https://gitee.com/hawk86104',
     state: 'active',
     creatTime: '2025-04-18',
-    updateTime: '2025-10-22',
+    updateTime: '2025-11-04',
     require: ['basic','digitalCity','floor','UIdemo','industry4','water'],
     tvtstore: 'LICENSE',
     preview: [
