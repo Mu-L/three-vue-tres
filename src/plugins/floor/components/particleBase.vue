2b4cae92a845eae64faa83bb328aebb91f3a8e07
hawk86104 <hawk86104@163.com>
2025-11-11 11:10:45 +0800

    fix: 增加 监听 粒子系统对长宽高的更改

--- src/plugins/floor/components/particleBase.vue
+++ src/plugins/floor/components/particleBase.vue
@@ -4,11 +4,12 @@
  * @Autor: 地虎降天龙
  * @Date: 2025-08-07 16:27:18
  * @LastEditors: 地虎降天龙
- * @LastEditTime: 2025-09-23 17:15:18
+ * @LastEditTime: 2025-11-11 11:04:22
 -->
 <script setup lang="ts">
+import { ref, watch } from 'vue'
 import { UseTexture } from '@tresjs/cientos'
-import { Precipitation } from '@tresjs/cientos'
+import Precipitation from 'PLS/basic/components/forCientos/Precipitation/index.vue'
 
 const props = withDefaults(
     defineProps<{
@@ -34,25 +35,21 @@ const props = withDefaults(
         opacity: 1.0,
     },
 )
+
+const array = ref([props.areaX, props.areaY, props.areaZ])
+watch(
+    () => [props.areaX, props.areaY, props.areaZ],
+    ([areaX, areaY, areaZ]) => {
+        array.value = [areaX, areaY, areaZ]
+    },
+)
 </script>
 <template>
     <TresGroup>
-        <Suspense>
-            <UseTexture v-slot="{ state: texture }" path="./plugins/digitalCity/image/cilcle.png">
-                <Precipitation :renderOrder="2001"
-                    :position="[0, areaY / 2, 0]"
-                    :speed="speed"
-                    :color="color"
-                    :area="[areaX, areaY, areaZ]"
-                    :count="count"
-                    :depthWrite="false"
-                    :randomness="randomness"
-                    :size="size"
-                    transparent
-                    :opacity="opacity"
-                    :map="texture"
-                />
-            </UseTexture>
-        </Suspense>
+        <UseTexture v-slot="{ state: texture }" path="./plugins/digitalCity/image/cilcle.png">
+            <Precipitation :renderOrder="2001" :position="[0, areaY / 2, 0]" :speed="speed" :color="color" :area="array"
+                :count="count" :depthWrite="false" :randomness="randomness" :size="size" transparent :opacity="opacity"
+                :map="texture" />
+        </UseTexture>
     </TresGroup>
 </template>
