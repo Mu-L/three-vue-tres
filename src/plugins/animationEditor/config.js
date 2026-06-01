83b9db935896c0be21d9db2dc06dd65445e3b9fd
hawk86104 <hawk86104@163.com>
2026-06-01 15:57:38 +0800

    no message

--- src/plugins/animationEditor/config.js
+++ src/plugins/animationEditor/config.js
@@ -4,12 +4,14 @@
  * @Autor: 地虎降天龙
  * @Date: 2026-05-28 12:16:24
  * @LastEditors: 地虎降天龙
- * @LastEditTime: 2026-05-28 15:31:34
+ * @LastEditTime: 2026-06-01 15:56:09
  */
 export default {
     name: 'animationEditor',
     title: '动画编辑器',
-    intro: '基于tvt.js的动画编辑器，支持动画编辑、动画播放、动画导出等功能',
+    intro: `基于tvt.js的动画编辑器，支持动画编辑、动画播放、动画导出等功能<br>
+    插件详情：<a style="color: #5384ff;" href="https://www.icegl.cn/tvtstore/animationEditor.html" target="_blank">icegl.cn/tvtstore/animationEditor</a><br>
+    `,
     version: '1.0.0',
     author: 'Jsonco and Sunny',
     website: "https://www.icegl.cn/ask/experts.html",
@@ -19,7 +21,7 @@ export default {
     preview: [
         {
             src: 'plugins/animationEditor/preview/预览图.png', type: 'img', name: 'index', title: '动画编辑器', disableFPSGraph: true,
-            disableSrcBtn: true, url: 'https://animationEditor.icegl.cn/#/plugins/animationEditor/index',
+            disableSrcBtn: true, url: 'https://animationeditor.icegl.cn/#/plugins/animationEditor/index',
         },
 
     ],
